4e9e933fa3492641fc3e9f5bd64d27edcaca2001
Nikhil <40881492+gsvn32@users.noreply.github.com>
2024-06-15 22:18:38 -0500

    Update analysis.py

--- assignment1/student-performance/src/analysis.py
+++ assignment1/student-performance/src/analysis.py
@@ -7,7 +7,7 @@ df['math score'].plot.hist()
 plt.xlabel('Math Score')
 plt.ylabel('Frequency')
 plt.title('Distribution of Math Scores')
-plt.savefig('math_scores.png')
+plt.savefig('../results/math_scores.png')
 
 # 2. Correlation between math and parental level of education
 # Analysis: This visualization shows the relationship between math and parental level of education. It allows us to see if there is a positive or negative correlation between the two variables.
@@ -15,7 +15,7 @@ df.plot.scatter(x='parental level of education',y='math score', )
 plt.xlabel('parental level of education')
 plt.ylabel('Math Score')
 plt.title('Correlation between Math and parental level of education')
-plt.savefig('scatter_plot_math_vs_parent_lvl_edu.png')
+plt.savefig('../results/scatter_plot_math_vs_parent_lvl_edu.png')
 
 # 3. Boxplot of writing scores by gender
 # Analysis: This visualization shows the distribution of writing scores by gender. It allows us to see the median, quartiles, and outliers for each group.
@@ -23,7 +23,7 @@ df.boxplot(column='writing score', by='gender')
 plt.xlabel('Gender')
 plt.ylabel('Writing Score')
 plt.title('Boxplot of Writing Scores by Gender')
-plt.savefig('bxplot_gender_vs_w_score.png')
+plt.savefig('../results/bxplot_gender_vs_w_score.png')
 
 # 4. Bar chart of average scores by race/ethnicity
 # Analysis: This visualization shows the average math scores for each racial/ethnic group. It allows us to see if there are any significant differences in average scores between groups.
@@ -31,7 +31,7 @@ df.groupby('race/ethnicity')['math score'].mean().plot.bar()
 plt.xlabel('Race/Ethnicity')
 plt.ylabel('Average Math Score')
 plt.title('Average Math Scores by Race/Ethnicity')
-plt.savefig('bar_avg_math_by_race.png')
+plt.savefig('../results/bar_avg_math_by_race.png')
 
 
 # 5. Pie chart of lunch type
@@ -40,4 +40,4 @@ df['lunch'].value_counts().plot.pie()
 plt.xlabel('Lunch Type')
 plt.ylabel('Frequency')
 plt.title('Pie Chart of Lunch Type')
-plt.savefig('pie_lunch-type.png')
+plt.savefig('../results/pie_lunch-type.png')
